cd2d2636a934e2ddb06fcc21c048deec21079d4e
Matsuel <matsuel0308@gmail.com>
2022-11-30 23:31:52 +0100

    hom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import Home from './components/home/Home';
 import './App.css';
 
 const App = () =>{
   return (
     <>
     <Header/>
+      <div className='main'>
+        <Home/>
+      </div>
     </>
   )
 }
