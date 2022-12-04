65beb55d27b5ce6f4f8f195ad014dfc0f28c5b21
lmatheo <matheo.lang@ynov.com>
2022-12-04 17:39:30 +0100

    contact

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header/Header';
 import Home from './components/home/Home';
 import About from './components/About/About';
 import Skills from './components/Skills/Skills';
+import Contact from './components/Contact/Contact';
 import './App.css';
 
 const App = () =>{
@@ -13,6 +14,7 @@ const App = () =>{
         <Home/>
         <About/>
         <Skills/>
+        <Contact/>
       </div>
     </>
   )
