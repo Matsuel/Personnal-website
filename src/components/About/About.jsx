822f620820b2c621ce06a2e6dad4eb403b821de4
Matsuel <matsuel0308@gmail.com>
2022-12-08 17:25:19 +0100

    about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -18,12 +18,10 @@ const About = () => {
                 <Info/>
 
                 <p className="about_description">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sh
-                eets containing Lorem Ipsum passages, and more recently with deskto
-                p publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                I am a computer science student, I am currently in my first year, I want to be a back-end web developer in the future, design is not my cup of tea.
                 </p>
 
-                <a download="" href={CV} className="button button--flex">Download CV
+                <a download="" className="button button--flex">Download CV (soon)
                 <svg
                 class="button_icon"
                 xmlns="http://www.w3.org/2000/svg"
--- src/components/About/info.jsx
+++ src/components/About/info.jsx
@@ -6,13 +6,13 @@ const Info = () => {
         <div className="about_box">
         <i class='bx bx-award about_icon'></i>
             <h3 className="about_title">Experience</h3>
-            <span className="about_subtitle">8 years working</span>
+            <span className="about_subtitle">0.5 years working</span>
         </div>
 
         <div className="about_box">
         <i class='bx bx-briefcase-alt about_icon'></i>
             <h3 className="about_title">Completed</h3>
-            <span className="about_subtitle">48+ Projects</span>
+            <span className="about_subtitle">10+ Projects</span>
         </div>
 
         <div className="about_box">
