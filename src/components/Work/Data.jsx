087486635bed5e331d5f8624e872da19f23c188e
Matsuel <matsuel0308@gmail.com>
2022-12-08 17:59:27 +0100

    projects

--- src/components/Work/Data.jsx
+++ src/components/Work/Data.jsx
@@ -1,30 +1,33 @@
 import Work1 from "../../assets/work1.jpg";
+import Site from "../../assets/site.png";
+import Taquin from "../../assets/taquin.png";
+import Classic from "../../assets/classic.png";
 
 export const projectsData= [
     {
         id:1,
-        image: Work1,
+        image: Site,
         title: 'My site',
         category: 'web',
         link:'https://github.com/Matsuel/Personnal-website.git',
     },
     {
         id:2,
-        image: Work1,
+        image: "",
         title: 'Battle card games',
         category: 'python',
         link:'https://github.com/Matsuel/Jeux-de-Bataille-POO-Python.git',
     },
     {
         id:3,
-        image: Work1,
+        image: Taquin,
         title: 'Puzzle',
         category: 'python',
         link:'https://github.com/Matsuel/Taquin-Python.git',
     },
     {
         id:4,
-        image: Work1,
+        image: Classic,
         title: 'Classic-Hangman',
         category: 'golang',
         link:'https://github.com/Matsuel/hangman-classic.git',
